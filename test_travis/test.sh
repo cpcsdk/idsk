bd599a65003ef89fdb0c664ed41d01537a376029
Romain Giot <romain.giot@u-bordeaux.fr>
2018-05-18 18:14:07 +0200

    Do not try to build iDSK in the test script; assume it is already built

--- test_travis/test.sh
+++ test_travis/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+IDSK=../iDSK
+
 function clean {
 	rm test.dsk
 }
@@ -8,7 +10,7 @@ function test_create {
 	echo test_create
 	clean
 
-	./iDSK test.dsk -n
+	$(IDSK) test.dsk -n
 	test -e test.dsk || exit -1
 
 }
@@ -18,22 +20,21 @@ function test_ascii {
 	clean
 
 	echo "HELLO WORLD" > /tmp/hello.asc
-	./iDSK test.dsk -n
+	$(IDSK) test.dsk -n
 
 	echo " > Add file"
-	./iDSK test.dsk -i /tmp/hello.asc -t 0 || exit -1
+	$(IDSK) test.dsk -i /tmp/hello.asc -t 0 || exit -1
 
 	echo " > Check file presence"
-	./iDSK test.dsk -l | grep "HELLO   .ASC 0" || exit -1
+	$(IDSK) test.dsk -l | grep "HELLO   .ASC 0" || exit -1
 
 	echo " > Retreive file"
-	./iDSK test.dsk -g "HELLO.ASC" && test -e HELLO.ASC || exit -1
+	$(IDSK) test.dsk -g "HELLO.ASC" && test -e HELLO.ASC || exit -1
 
 	echo " > Check file content"
 	test "$(cat HELLO.ASC)" = "HELLO WORLD"
 }
 
-cmake .. \
-	&& make \
-	&& test_create \
-	&& test_ascii
+
+test_create && \
+test_ascii
